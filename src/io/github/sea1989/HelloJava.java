341b6515dde76ca3484ec5f80f94f0867ed14ce3
sea1989 <137886873@qq.com>
2018-11-28 19:52:16 +0800

    1234

---
+++ src/io/github/sea1989/HelloJava.java
@@ -0,0 +1,7 @@
+package io.github.sea1989;
+
+public class HelloJava {
+	public static void main(String[] args) {
+		System.out.println("hello java");
+	}
+}
---
+++ src/io/github/sea1989/HelloWorld.java
@@ -0,0 +1,7 @@
+package io.github.sea1989;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+}
